5b73b6f9884134c09f23fe8f7c5814f4066a5f85
ArulRajselva <arulstarline2016@gmail.com>
2022-05-27 10:55:44 +0530

    git program codes

---
+++ src/test/java/com/tcs/GitProgram.java
@@ -0,0 +1,22 @@
+package com.tcs;
+
+public class GitProgram {
+
+	
+	private void method1() {
+		System.out.println("1");
+
+	}
+
+	private void method2() {
+		System.out.println("2");
+
+	}
+
+	private void method3() {
+		System.out.println("3");
+
+	}
+
+}
+
